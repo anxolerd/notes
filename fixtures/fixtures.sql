130503ff612c82cb479fd9d759537d1d8f2c8c5a
anxolerd <anxolerd@outlook.com>
2016-12-19 04:04:15 +0200

    Fix sequences after fixtures load

--- fixtures/fixtures.sql
+++ fixtures/fixtures.sql
@@ -7,10 +7,12 @@ INSERT INTO "user" (
   (1, 'admin', 'admin', 'Admin', NULL, 'Admin', '{2, 5}', '{"admin", "writer"}'),
   (2, 'writer', 'writer', 'Writer', NULL, 'Writer', '{5, 2}', '{"writer"}')
 ;
+ALTER SEQUENCE user_id_seq RESTART WITH 3;
 
 INSERT INTO "category" (id, name, allowed_roles) VALUES
   (1, 'top_secret', '{"admin"}'),
   (2, 'public', '{"writer", "admin"}')
 ;
+ALTER SEQUENCE category_id_seq RESTART WITH 3;
 
 END;
